7bf1a9c2093ec4c137aa2929f8766f9c5d242ba8
Yeahoon-Kim <apple8718@korea.ac.kr>
2022-04-27 17:01:48 +0900

    Version 1.

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+https://badayak.com/entry/C언어-소켓에-IP주소와-포트번호-지정-함수-bind
+https://www.tuwlab.com/ece/27193
---
+++ src/client/main.c
@@ -0,0 +1,30 @@
+#include "chatClient.h"
+
+/*
+* Socket 통신을 위한 Client 프로그램 작성
+* Server : socket() -> bind() -> listen() -> accept() -> send() -> recv()
+* Client : socket() ->                connect() ->       recv() -> send() -> close()
+*/
+int main(int argc, char* argv[]) {
+    int clientSocketDescriptor;
+
+    if(argc != 3) {
+        printUsageError();
+        exit(EXIT_FAILURE);
+    }
+
+    if(clientSocketSetting(&clientSocketDescriptor, argv[1], argv[2]) == SETTING_FAILURE) {
+        exit(EXIT_FAILURE);
+    }
+
+    if(chattingClient(clientSocketDescriptor)) {
+        exit(EXIT_FAILURE);
+    }
+
+    if(close(clientSocketDescriptor)) {
+        perror(CLOSE_ERROR_MSG);
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
